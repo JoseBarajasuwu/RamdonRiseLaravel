c828cd1b938ac67fe0b70060bc273bb5ad271ae8
JoseBarajasuwu <dospuntosuve0@gmail.com>
2026-01-08 15:06:27 -0600

    Se agrega la rifa

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -10,8 +10,8 @@ class LoginController extends Controller
 {
     public function login(Request $request)
     {
-        $Password = $request->Pass;
-        $NombreUsuario = $request->NombreUs;
+        $Password = $request->input('password');
+        $NombreUsuario = $request->input('usuario');
         try {
             $usuarios = DB::select('SELECT
             "UsuarioID"
@@ -21,10 +21,15 @@ class LoginController extends Controller
             "Contrasenia" = ? 
         AND "NombreUsuario" = ?', [$Password, $NombreUsuario]);
             if (!empty($usuarios)) {
-                return $usuarios;
+                $data = [
+                    "UsuarioID" => 1
+                ];
+                return json_encode($data);
             } else {
-                $usuarioID [] = array("UsuarioID" => 0);
-                return $usuarioID;
+                $data = [
+                    "UsuarioID" => 0
+                ];
+                return json_encode($data);
             }
         } catch (\Throwable $th) {
             return response()->json([], 500);
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\EstudioController;
+use App\Http\Controllers\RifaController;
 
 //******************************************************//
 Route::POST('/login', [LoginController::class, 'login']);
@@ -15,4 +16,11 @@ Route::POST('/estudio/crear-estudio', [EstudioController::class,'CrearEstudio'])
 Route::POST('/estudio/mostrar-estudio', [EstudioController::class,'MostrarEstudios']);
 Route::POST('/estudio/datos-estudio', [EstudioController::class,'MostrarDatosEstudios']);
 Route::POST('/estudio/eliminar-estudio', [EstudioController::class,'EliminarEstudio']);
+//**************************************************************************************//
+
+//**************************************************************************************//
+Route::POST('/estudio/mostrar-boletos', [RifaController::class,'MostrarBoletos']);
+Route::POST('/estudio/guardar-boleto', [RifaController::class,'GuardarBoletos']);
+Route::POST('/estudio/editar-boleto', [RifaController::class,'EditarBoleto']);
+Route::POST('/estudio/no-ganador-boleto', [RifaController::class,'NoGanadorBoleto']);
 //**************************************************************************************//
